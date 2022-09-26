9e7bbc8a272b0e88b11a05b3e1948bd187d4debc
Tristan Aly <tristan.aly@gmail.com>
2022-09-26 09:44:55 +0200

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by apprenant1 on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CoffeeApp
+//
+//  Created by apprenant1 on 26/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
